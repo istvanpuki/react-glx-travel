6a3d242233bc295e84cd9d834e0eaf27a9fc96b7
istvanpuki <istvan.puki92@gmail.com>
2022-11-15 21:12:40 +0100

    Hamburger menu done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import {Routes, Route} from "react-router-dom"
 
+import Home from "./routes/Home";
 
 function App() {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={<Home />} />
+
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import './Navbar.css'
+import {Link} from 'react-router-dom'
+
+import {FaBars, FaTimes} from 'react-icons/fa'
+
+const Navbar = () => {
+
+  const [click, setClick] = useState(false)
+  const handleClick = () => { setClick(!click) }
+
+  return (
+    <div className='header'>
+
+      <Link to='/'><h1>GLX TRVL</h1></Link>
+
+      <ul className={click ? `nav-menu active` : `nav-menu`}>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='pricing'>Pricing</Link>
+        </li>
+        <li>
+          <Link to='training'>Training</Link>
+        </li>
+        <li>
+          <Link to='contact'>Contact</Link>
+        </li>
+      </ul>
+
+      <div className='hamburger' onClick={handleClick}>
+        {click ?<FaTimes style={{color: '#fff'} } size={20}/>:<FaBars style={{color: '#fff'} } size={20}/> }
+          
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
+
+
